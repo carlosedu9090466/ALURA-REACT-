1e663207d7cad6758662933204e5287caa415778
carloscidrao <carloscidrao@seplag.mt.gov.br>
2026-08-19 15:48:23 -0400

    first_components

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+function Banner() {
+    // JSX
+    return (
+        // className para o mesmo criterio de classe em html e css
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do Organo" />
+        </header>
+    );
+}
+
+export default Banner;
