0343853d8bd8652709e486057c53e53919a77921
carloscidrao <carloscidrao@seplag.mt.gov.br>
2026-08-20 17:43:46 -0400

    props_inicio

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css';
 
-function Banner() {
+export const Banner = () => {
     // JSX
     return (
         // className para o mesmo criterio de classe em html e css
@@ -9,5 +9,3 @@ function Banner() {
         </header>
     );
 }
-
-export default Banner;
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) => {
+
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='Digite o seu nome' />
+        </div>
+    );
+}
+
+export default CampoTexto;
