860695b083e39ffe28c244b9c8c43945207f2a20
carloscidrao <carloscidrao@seplag.mt.gov.br>
2026-08-20 17:21:20 -0400

    deleted_imgd_arquivos_padrao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/Banner/Banner';
 
 function App() {
@@ -7,20 +5,6 @@ function App() {
     <div className="App">
       <Banner />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
