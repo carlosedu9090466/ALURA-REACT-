68e6803efab9874e582a2925cc6df222fcf2aacc
carloscidrao <carloscidrao@seplag.mt.gov.br>
2026-09-01 18:05:19 -0400

    feat:colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -2,6 +2,21 @@ import './Colaborador.css';
 
 const Colaborador = (props) => {
 
+    return (
+
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/carlosedu9090466.png' alt='' />
+            </div>
+
+            <div className='rodape'>
+                <h4>Carlos Eduardo</h4>
+                <h5>Dev Back-End</h5>
+            </div>
+
+        </div>
+
+    )
 }
 
 export default Colaborador;
